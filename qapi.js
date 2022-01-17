5ab73fdb4f09240b84600312e7cb9a1c0c802261
Andrey Zhuravlyov <crane.zhuravlyov@gmail.com>
2022-01-17 17:37:43 +0300

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"61e57bafa75dc7001e3016e8",
+				"61e57bafa75dc7001e3016eb"
+			]
+		},
+		"61e57bafa75dc7001e3016e8": {
+			"id": "61e57bafa75dc7001e3016e8",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"61e57bafa75dc7001e3016eb": {
+			"id": "61e57bafa75dc7001e3016eb",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import theme from "theme";
+import { Theme } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { RawHtml } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<RawHtml>
+			<style place={"endOfHead"} rawKey={"61e57bafa75dc7001e3016e1"}>
+				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
+			</style>
+		</RawHtml>
+	</Theme>;
+});
